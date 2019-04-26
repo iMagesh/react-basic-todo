220de14f1de20a1240685efab632b4c6133457c9
Magesh <magesh@sedintechnologies.com>
2019-04-26 13:39:48 +0530

    Modified to class component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
-function App() {
-  return (
-    <div class="container">
-      <h2>My Todo App</h2>
-      <TodoForm />
-      <TodoList />
-    </div>
-  );
+class App extends React.Component {
+  todos = [
+    "My first todo task",
+    "Watch Game of thrones next episode",
+    "Practice react todo application"
+  ];
+  render() {
+    return (
+      <div class="container">
+        <h2>My Todo App</h2>
+        <TodoForm />
+        <TodoList todos={this.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,19 +1,21 @@
 import React from "react";
 
-function TodoForm() {
-  return (
-    <div class="form-group">
-      <input
-        type="text"
-        class="form-control"
-        placeholder="Add todo"
-        aria-describedby="helpId"
-      />
-      <button type="button" name="" id="" class="btn btn-primary btn-block">
-        Add Todo
-      </button>
-    </div>
-  );
+class TodoForm extends React.Component {
+  render() {
+    return (
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Add todo"
+          aria-describedby="helpId"
+        />
+        <button type="button" name="" id="" class="btn btn-primary btn-block">
+          Add Todo
+        </button>
+      </div>
+    );
+  }
 }
 
 export default TodoForm;
