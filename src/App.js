7beba08a843b0b8af3b7a420f44d14f68da0136b
Magesh <magesh@sedintechnologies.com>
2019-04-26 13:06:17 +0530

    split the code into several components inside App

--- src/App.js
+++ src/App.js
@@ -4,24 +4,36 @@ function App() {
   return (
     <div class="container">
       <h2>My Todo App</h2>
-      <div class="form-group">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Add todo"
-          aria-describedby="helpId"
-        />
-        <button type="button" name="" id="" class="btn btn-primary btn-block">
-          Add Todo
-        </button>
-      </div>
-      <div>
-        <ul class="list-group">
-          <li class="list-group-item">My first todo task</li>
-          <li class="list-group-item">Watch Game of thrones next episode</li>
-          <li class="list-group-item">Practice react todo application</li>
-        </ul>
-      </div>
+      <TodoForm />
+      <TodoList />
+    </div>
+  );
+}
+
+function TodoForm() {
+  return (
+    <div class="form-group">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Add todo"
+        aria-describedby="helpId"
+      />
+      <button type="button" name="" id="" class="btn btn-primary btn-block">
+        Add Todo
+      </button>
+    </div>
+  );
+}
+
+function TodoList() {
+  return (
+    <div>
+      <ul class="list-group">
+        <li class="list-group-item">My first todo task</li>
+        <li class="list-group-item">Watch Game of thrones next episode</li>
+        <li class="list-group-item">Practice react todo application</li>
+      </ul>
     </div>
   );
 }
