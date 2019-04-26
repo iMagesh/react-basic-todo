3ee262d3a7239e86d9a7134eeaf3936a9ccbb458
Magesh <magesh@sedintechnologies.com>
2019-04-26 16:25:22 +0530

    mark complete function added

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,20 @@
 import React from "react";
 
+const toggleDone = event => {
+  if (event.target.style.textDecoration === "line-through") {
+    event.target.style.textDecoration = "";
+  } else {
+    event.target.style.textDecoration = "line-through";
+  }
+};
+
 function TodoList(props) {
   return (
     <div>
       <ul className="list-group">
         {props.todos.map((todo, i) => {
           return (
-            <li key={i} className="list-group-item">
+            <li key={i} className="list-group-item" onClick={toggleDone}>
               {todo}
             </li>
           );
