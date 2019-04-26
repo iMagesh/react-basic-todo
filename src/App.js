52dfebe609795f9a8b1856ead905449e40cf51dd
Magesh <magesh@sedintechnologies.com>
2019-04-26 12:50:10 +0530

    Added bootstrap design to make it a bit attractive

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div class="container">
       <h2>My Todo App</h2>
-      <div>
-        <input type="text" placeholder="Add todo" />
-        <button>Add Todo</button>
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Add todo"
+          aria-describedby="helpId"
+        />
+        <button type="button" name="" id="" class="btn btn-primary btn-block">
+          Add Todo
+        </button>
       </div>
       <div>
-        <ul>
-          <li>My first todo task</li>
-          <li>Watch Game of thrones next episode</li>
-          <li>Practice react todo application</li>
+        <ul class="list-group">
+          <li class="list-group-item">My first todo task</li>
+          <li class="list-group-item">Watch Game of thrones next episode</li>
+          <li class="list-group-item">Practice react todo application</li>
         </ul>
       </div>
     </div>
